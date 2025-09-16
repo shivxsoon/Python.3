465d5499393aec514e2cb5bfff7ffa005fc225ef
shivxsoon <mshivaryashashank@gmail.com>
2025-09-16 18:37:04 +0800

    Add files via upload

---
+++ Trignometic Functions.py
@@ -0,0 +1,15 @@
+import math
+angle_deg = float(input("Enter angle in degrees"))
+angle_rad = math.radians(angle_deg)
+
+rad_sin = math.sin(angle_rad)
+deg_sin = math.sin(angle_deg)
+
+rad_tan = math.tan(angle_rad)
+deg_tan = math.tan(angle_deg)
+
+rad_cos = math.cos(angle_rad)
+deg_cos = math.cos(angle_deg)
+
+rad_cot = math.cot(angle_rad)
+deg_cot = math.cot(angle_deg)
