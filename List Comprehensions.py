3fb36a7eca1b2fde96d76b3652d4bca1b3987eb0
shivxsoon <mshivaryashashank@gmail.com>
2025-12-28 18:10:05 +0800

    lIST Comp
    List Comp

---
+++ List Comprehensions.py
@@ -0,0 +1,25 @@
+# 1. Create a list of squares of numbers from 1 to 10
+squares = [x**2 for x in range(1, 11)]
+print("Squares:", squares)
+
+# 2. Get only even numbers from a list
+numbers = list(range(1, 21))
+evens = [x for x in numbers if x % 2 == 0]
+print("Evens:", evens)
+
+# 3. Convert words to uppercase
+words = ["apple", "banana", "cherry"]
+upper_words = [w.upper() for w in words]
+print("Uppercase words:", upper_words)
+
+Mankar is inviting you to a scheduled Zoom meeting.
+
+Topic: sam subsitute class
+
+Time: Oct 1, 2025 04:30 PM IST
+
+https://us04web.zoom.us/j/77336113565?pwd=sXzJhC6MyEu2XV0pVsba6F8z2uxBsN.1
+
+Meeting ID: 773 3611 3565
+
+Passcode: K0G6CM
